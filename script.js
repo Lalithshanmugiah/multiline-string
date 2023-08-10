4e75fc092952e675b63f5ad9608655cea08a4665
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-10 12:14:14 +0530

    new commit

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const string ='multiline string using +'+
 'two more type are there in multiline str'
 
 console.log(string);
+
+//a multiline strings using \
+
+
+const message = 'new string \n \
+another str\n \
+end.'
+
+console.log(message);
